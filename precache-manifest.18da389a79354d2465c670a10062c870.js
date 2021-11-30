3d743957cf393ac484cf42c96fcfa0b35617b4eb
iosifache <georgeiosif@live.com>
2021-12-01 00:41:36 +0200

    Updates

---
+++ precache-manifest.18da389a79354d2465c670a10062c870.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "20963fe9d2fae05e41de",
+    "url": "/css/app.dad0c7ad.css"
+  },
+  {
+    "revision": "20963fe9d2fae05e41de",
+    "url": "/js/app.40949d74.js"
+  },
+  {
+    "revision": "cc11c4efe4bd1815ee36",
+    "url": "/js/chunk-vendors.b81c82df.js"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "5fcb8482fb64d5d24a96e33c1dc148d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8594f48ee3fe5208fc44cd146684a523",
+    "url": "/img/logo.svg"
+  },
+  {
+    "revision": "7a7a71ec9362431e07c898aee1fd7e6a",
+    "url": "/img/preview.png"
+  }
+];
